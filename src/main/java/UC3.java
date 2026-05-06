9afab24ea502a8366097a355da393e441e4fdc52
tk7622 <tk622@srmist.edu.in>
2026-05-06 14:26:16 +0530

    uc3

--- src/main/java/UC3.java
+++ src/main/java/UC3.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class UC2 {
+public class UC3 {
 
     static char[][] board = new char[3][3];
 
@@ -13,6 +13,8 @@ public class UC2 {
         performToss();
         printBoard();
 
+        int slot = getUserSlot();
+        System.out.println("You selected slot: " + slot);
     }
 
     public static void initializeBoard() {
@@ -54,4 +56,12 @@ public class UC2 {
         }
     }
 
+    public static int getUserSlot() {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = sc.nextInt();
+
+        return slot;
+    }
 }
