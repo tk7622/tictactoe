56cb78eef6883fd35fb285c3e9450f02d1155f48
tk7622 <tk622@srmist.edu.in>
2026-05-06 14:24:39 +0530

    UC2

---
+++ src/main/java/UC2.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+public class UC2 {
+
+    static char[][] board = new char[3][3];
+
+    static char player1Symbol;
+    static char player2Symbol;
+    static char currentPlayerSymbol;
+
+    public static void main(String[] args) {
+        initializeBoard();
+        performToss();
+        printBoard();
+
+    }
+
+    public static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '_';
+            }
+        }
+    }
+
+
+    public static void performToss() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+            currentPlayerSymbol = player1Symbol;
+
+            System.out.println("Player 1 starts and is 'X'");
+            System.out.println("Player 2 is 'O'");
+        } else {
+            player1Symbol = 'O';
+            player2Symbol = 'X';
+            currentPlayerSymbol = player2Symbol;
+
+            System.out.println("Player 2 starts and is 'X'");
+            System.out.println("Player 1 is 'O'");
+        }
+    }
+
+    public static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
